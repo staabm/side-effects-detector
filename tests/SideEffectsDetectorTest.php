e7e7097832f39d10d77fab31835b84bb6b1f3a3a
Markus Staab <maggus.staab@googlemail.com>
2024-09-28 19:08:54 +0200

    simplify tests

--- tests/SideEffectsDetectorTest.php
+++ tests/SideEffectsDetectorTest.php
@@ -10,64 +10,34 @@ class SideEffectsDetectorTest extends TestCase {
     /**
      * @dataProvider dataHasSideEffects
      */
-    public function testHasSideEffects(string $code, ?bool $expected): void {
+    public function testHasSideEffects(string $code, ?bool $expected, ?bool $expectedIgnoreOutput): void {
         $detector = new SideEffectsDetector();
         self::assertEquals($expected, $detector->hasSideEffects($code));
+
+        self::assertEquals($expectedIgnoreOutput, $detector->hasSideEffects($code, true));
     }
 
     static public function dataHasSideEffects():iterable
     {
-        yield ['<?php echo "Hello World";', true];
-        yield ['<?php print("Hello World");', true];
-        yield ['<?php fopen("file.txt");', true];
-        yield ['<?php version_compare(PHP_VERSION, "8.0", ">=") or die("skip because attributes are only available since PHP 8.0");', true];
-        yield ['<?php version_compare(PHP_VERSION, "8.0", ">=") or echo("skip because attributes are only available since PHP 8.0");', true];
-        yield ['<?php exit(0);', true];
-        yield ['<?php eval($x);', true];
-        yield ['<?php global $x; $x = false;', true];
-        yield ['<?php goto somewhere;', true];
-        yield ['<?php include "some-file.php";', true];
-        yield ['<?php include_once "some-file.php";', true];
-        yield ['<?php require "some-file.php";', true];
-        yield ['<?php require_once "some-file.php";', true];
-        yield ['<?php throw new RuntimeException("foo");', true];
-        yield ['<?php unknownFunction($x);', null];
-        yield ['<?php unset($x);', true];
-        yield ['<?php (unset)$x;', true];
-        yield ['<?php new SomeClass();', true];
-        yield ['<?php function abc() {}', true];
-        yield ['<?php class abc() {}', true];
-    }
-
-    /**
-     * @dataProvider dataHasSideEffectsIgnoreOutput
-     */
-    public function testHasSideEffectsIgnoreOutput(string $code, ?bool $expected): void {
-        $detector = new SideEffectsDetector();
-        self::assertEquals($expected, $detector->hasSideEffects($code, true));
-    }
-
-    static public function dataHasSideEffectsIgnoreOutput():iterable
-    {
-        yield ['<?php echo "Hello World";', false];
-        yield ['<?php print("Hello World");', false];
-        yield ['<?php fopen("file.txt");', true];
-        yield ['<?php version_compare(PHP_VERSION, "8.0", ">=") or die("skip because attributes are only available since PHP 8.0");', true];
-        yield ['<?php version_compare(PHP_VERSION, "8.0", ">=") or echo("skip because attributes are only available since PHP 8.0");', false];
-        yield ['<?php exit(0);', true];
-        yield ['<?php eval($x);', true];
-        yield ['<?php global $x; $x = false;', true];
-        yield ['<?php goto somewhere;', true];
-        yield ['<?php include "some-file.php";', true];
-        yield ['<?php include_once "some-file.php";', true];
-        yield ['<?php require "some-file.php";', true];
-        yield ['<?php require_once "some-file.php";', true];
-        yield ['<?php throw new RuntimeException("foo");', true];
-        yield ['<?php unknownFunction($x);', null];
-        yield ['<?php unset($x);', true];
-        yield ['<?php (unset)$x;', true];
-        yield ['<?php new SomeClass();', true];
-        yield ['<?php function abc() {}', true];
-        yield ['<?php class abc() {}', true];
+        yield ['<?php echo "Hello World";', true, false];
+        yield ['<?php print("Hello World");', true, false];
+        yield ['<?php fopen("file.txt");', true, true];
+        yield ['<?php version_compare(PHP_VERSION, "8.0", ">=") or die("skip because attributes are only available since PHP 8.0");', true, true];
+        yield ['<?php version_compare(PHP_VERSION, "8.0", ">=") or echo("skip because attributes are only available since PHP 8.0");', true, false];
+        yield ['<?php exit(0);', true, true];
+        yield ['<?php eval($x);', true, true];
+        yield ['<?php global $x; $x = false;', true, true];
+        yield ['<?php goto somewhere;', true, true];
+        yield ['<?php include "some-file.php";', true, true];
+        yield ['<?php include_once "some-file.php";', true, true];
+        yield ['<?php require "some-file.php";', true, true];
+        yield ['<?php require_once "some-file.php";', true, true];
+        yield ['<?php throw new RuntimeException("foo");', true, true];
+        yield ['<?php unknownFunction($x);', null, null];
+        yield ['<?php unset($x);', true, true];
+        yield ['<?php (unset)$x;', true, true];
+        yield ['<?php new SomeClass();', true, true];
+        yield ['<?php function abc() {}', true, true];
+        yield ['<?php class abc() {}', true, true];
     }
 }
